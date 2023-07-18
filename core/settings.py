f4d87bbdcaa74cc784a425c9d69a5fbb763dd8ba
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-07-18 19:52:22 +0600

    Add app in installed_apps

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My Apps
+    'type_test.apps.TypeTestConfig',
 ]
 
 MIDDLEWARE = [
