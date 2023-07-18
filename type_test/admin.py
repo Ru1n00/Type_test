deaae612b031f7d1b8ba558ef297bc0ddec9bcb3
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-07-18 20:02:01 +0600

    Modify Admin

--- type_test/admin.py
+++ type_test/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile, Test, CompletedTest
 
 # Register your models here.
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author', 'added_by', 'created', 'updated')
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'name', 'email', 'avg_speed', 'avg_accuracy', 'match_played', 'created', 'updated')
+
+class CompletedTestAdmin(admin.ModelAdmin):
+    list_display = ('id', 'played_by', 'test', 'speed', 'accuracy', 'created', 'updated')
