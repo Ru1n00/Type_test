abcece5c6142b8ef4c38c5cafd069c6e7566c6af
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-07-18 20:49:19 +0600

    Create sign_in and sign_out views

--- type_test/urls.py
+++ type_test/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('api/test-api', views.test_api, name='test-api'),
     path('api/parse-last-10', views.parse_last_10_tests, name='parse-last-10'),
     path('api/post-score', views.post_score, name='post-score'),
-
+    path('sign-in', views.sign_in, name='sign-in'),
+    path('sign-out', views.sign_out, name='sign-out'),
 ]
