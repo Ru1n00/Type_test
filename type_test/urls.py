07cd8dd7646e27ddada746b45518636071697d9d
Ru1n00 <ruhul.amin.tnt@gmail.com>
2023-07-20 16:53:18 +0600

    Add signup url

--- type_test/urls.py
+++ type_test/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('api/post-score', views.post_score, name='post-score'),
     path('sign-in', views.sign_in, name='sign-in'),
     path('sign-out', views.sign_out, name='sign-out'),
+    path('sign-up', views.sign_up, name='sign-up'),
 ]
